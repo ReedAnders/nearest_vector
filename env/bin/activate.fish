e799188561dabcaa92733e628e46c8f7a1b33bda
Ubuntu <ubuntu@ip-172-31-43-147.us-west-2.compute.internal>
2017-04-20 21:52:07 +0000

    running

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user/distributed_health_app/wordcount/env"
+set -gx VIRTUAL_ENV "/home/ubuntu/nearest_vector/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
