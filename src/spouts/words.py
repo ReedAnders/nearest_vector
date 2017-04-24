e1bbf77e1554e60008ee02f4c14e7daa446b47a1
ReedAnders <rean8874@colorado.edu>
2017-04-24 17:02:06 -0600

    adding logs

--- src/spouts/words.py
+++ src/spouts/words.py
@@ -10,7 +10,12 @@ class IndexSpout(Spout):
         self.matrix = ((x,y) for (x,y) in enumerate(np.random.rand(10000,20)))
 
     def next_tuple(self):
-        vector = next(self.matrix)
+        try:
+            vector = next(self.matrix)
 
-        for index_id, pair in enumerate(zip(vector[1], self.query)):
-            self.emit([pair, vector[0]])
+            for index_id, pair in enumerate(zip(vector[1], self.query)):
+                self.emit([pair, vector[0]])
+
+            self.logger.info("vector id [{:,}]".format(vector[0]))
+        except:
+            pass
