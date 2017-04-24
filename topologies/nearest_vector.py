cd380edb8774d0b30bb5f38c65fb404333f68b47
ReedAnders <rean8874@colorado.edu>
2017-04-24 17:41:14 -0600

    updating topology

--- topologies/nearest_vector.py
+++ topologies/nearest_vector.py
@@ -11,7 +11,7 @@ from spouts.words import IndexSpout
 class NearestVectors(Topology):
     vector_spout = IndexSpout.spec()
     index_bolt = PairProcessBolt.spec(inputs={vector_spout: Grouping.SHUFFLE},
-                                    par=8)
+                                    par=8, outputs=['pair', 'vector_id'])
     sum_bolt = VectorSumBolt.spec(inputs={index_bolt: Grouping.fields('vector_id')},
                                     par=8)
     final_bolt = NearestBolt.spec(inputs={sum_bolt: Grouping.fields('final')},
